0792b379113d4b5aca252bd4162da27629d131f9
kylecox1 <33430803+kylecox1@users.noreply.github.com>
2021-11-03 18:21:00 -0400

    First method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 
 namespace Lab07
@@ -7,6 +8,24 @@ namespace Lab07
     {
         static void Main(string[] args)
         {
+
+
+            Console.WriteLine("Thanks, press any to exit.");
+            Console.ReadKey();
+        }
+
+
+        public static bool ValidateName()
+        {
+            string pattern = "^[A-Z][a-z]{1,29}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(pattern))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
         }
     }
 }
