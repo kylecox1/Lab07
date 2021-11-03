1003a83fe4d83a63d7bce97599db43d31398774e
kylecox1 <33430803+kylecox1@users.noreply.github.com>
2021-11-03 18:27:21 -0400

    fix method, test in main

--- Program.cs
+++ Program.cs
@@ -8,21 +8,31 @@ namespace Lab07
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Please enter a name: ");
+            string input = Console.ReadLine();
+            if (ValidateName(input))
+            {
+                Console.WriteLine("Name is valid!");
+            } 
+            else
+            {
+                Console.WriteLine("Name is not valid!");
+            }
 
             Console.WriteLine("Thanks, press any to exit.");
             Console.ReadKey();
         }
 
 
-        public static bool ValidateName()
+        public static bool ValidateName(string name)
         {
             string pattern = "^[A-Z][a-z]{1,29}$";
             Regex regex = new Regex(pattern);
-            if (regex.IsMatch(pattern))
+            if (regex.IsMatch(name))
             {
                 return true;
-            } else
+            } 
+            else
             {
                 return false;
             }
